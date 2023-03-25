baa45592634c9d1cf739758d3ea77786f6bebe4d
tomlazen <oismarilo@gmail.com>
2023-03-25 22:28:13 +0100

    added todos

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from core import Logic
-from test import runtime_test
+from test import runtime_test, runtime_monitoring
 
 GUI = Logic.gui
 
@@ -10,7 +10,7 @@ def master_mainloop():
     Logic.logic_player()
     Logic.out_of_deck()
     Logic.check_the_status()
-    runtime_test(verbose=False)
+    runtime_test()
 
     # Redraw screen only when there is some change during game.
     if Logic.past_players_nr_of_cards != len(Logic.player_hands.my_cards()) \
@@ -25,12 +25,20 @@ def master_mainloop():
         GUI.changer_icons_()
         GUI.round_end_screen()
         GUI.total_end_screen()
+        runtime_monitoring(verbose=True)
 
-    GUI.after(1500, master_mainloop)  # run again after X ms
+    GUI.after(2000, master_mainloop)  # run again after X ms
 
 
 if __name__ == "__main__":
     GUI.start_screen()
-    GUI.after(2000, master_mainloop)  # run first time after X ms
+    GUI.after(3000, master_mainloop)  # run first time after X ms
     GUI.mainloop()
+
+# TODO: sometimes there is a bug where pc plays a seven, two
+#   cards are automatically assigned to player from deck and still
+#   it is players turn.
+# TODO: flickering was largely removed but still happens from time to time.
+#  Destroy only specific widgets at a time, not all.
+# TODO: make a log of every game, written into txt, for debugging reasons
 
